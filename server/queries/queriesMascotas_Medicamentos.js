a314deb3367977a69846d5bf473991f60cc69441
LuisRG42 <luisrg90523@gmail.com>
2024-11-21 18:34:34 -0400

    Server commit

--- server/queries/queriesMascotas_Medicamentos.js
+++ server/queries/queriesMascotas_Medicamentos.js
@@ -158,11 +158,70 @@ const obtenerMedicamentosPorMascotaDesparasitante = async (id_mascota) => {
   }
 };
 
+//Funcion para crear Mascota_Medicamento
+const createMascotaMedicamento = async (id_mascota, id_medicamento, tipo, cantidad, fecha_dosis, fecha_refuerzo) => {
+  try {
+    const nuevaMascotaMedicamento = await Mascotas_Medicamentos.create({
+      id_mascota,
+      id_medicamento,
+      tipo,
+      cantidad,
+      fecha_dosis,
+      fecha_refuerzo
+    });
+    return nuevaMascotaMedicamento.toJSON();
+  } catch (error) {
+    console.error("Error al crear la relación entre mascota y medicamento:", error);
+    throw new Error('No se pudo crear la relación entre mascota y medicamento');
+  }
+};
+
+//Funcion para actualizar Mascota_Medicamento
+const updateMascotaMedicamento = async (id_mascotaMedicamento, id_mascota, id_medicamento, tipo, cantidad, fecha_dosis, fecha_refuerzo) => {
+  try {
+    const mascotaMedicamento = await Mascotas_Medicamentos.findByPk(id_mascotaMedicamento);
+    if (!mascotaMedicamento) {
+      throw new Error('Relación entre mascota y medicamento no encontrada');
+    }
+    if (id_mascota) mascotaMedicamento.id_mascota = id_mascota;
+    if (id_medicamento) mascotaMedicamento.id_medicamento = id_medicamento;
+    if (tipo) mascotaMedicamento.tipo = tipo;
+    if (cantidad) mascotaMedicamento.cantidad = cantidad;
+    if (fecha_dosis) mascotaMedicamento.fecha_dosis = fecha_dosis;
+    if (fecha_refuerzo) mascotaMedicamento.fecha_refuerzo = fecha_refuerzo;
+    await mascotaMedicamento.save();
+    return { message: 'Relación entre mascota y medicamento actualizada con éxito' };
+  } catch (error) {
+    console.error('Error al actualizar la relación entre mascota y medicamento:', error);
+    return { message: 'Error al actualizar la relación entre mascota y medicamento' };
+  }
+};
+
+//Funcion para borrar Mascota_Medicamento
+const deleteMascotaMedicamento = async (id_mascotaMedicamento) => {
+  try {
+    const deletedMascotaMedicamento = await Mascotas_Medicamentos.destroy({
+      where: { id_mascotaMedicamento },
+    });
+
+    if (deletedMascotaMedicamento) {
+      return { message: 'Relación entre mascota y medicamento eliminada con éxito' };
+    } else {
+      return { message: 'Relación entre mascota y medicamento no encontrada' };
+    }
+  } catch (error) {
+    console.error('Error al eliminar la relación entre mascota y medicamento:', error);
+    return { message: 'Error al eliminar la relación entre mascota y medicamento', error };
+  }
+};
 
 module.exports = {
   obtenerMedicamentosPorMascotaVacuna,
   obtenerMedicamentosPorMascotaSupresor,
   obtenerMedicamentosPorMascotaDesparasitante,
+  createMascotaMedicamento,
+  updateMascotaMedicamento,
+  deleteMascotaMedicamento,
 };
   
   
--- server/routes/mascotas_medicamentosRoutes.js
+++ server/routes/mascotas_medicamentosRoutes.js
@@ -4,73 +4,97 @@ const router = express.Router();
 
 // Ruta para obtener los medicamentos de tipo VACUNA de una mascota por id_mascota
 router.get('/vacuna/:id_mascota', async (req, res) => {
-    const { id_mascota } = req.params;  // Obtenemos el id_mascota desde los parámetros de la URL
+    const { id_mascota } = req.params;  
     
     try {
-      // Llamamos a la función para obtener los medicamentos de tipo VACUNA para la mascota
       const medicamentos = await MascotasMedicamentosQueries.obtenerMedicamentosPorMascotaVacuna(id_mascota);
-  
       if (medicamentos.message) {
-        // Si no se encontraron resultados
         return res.status(404).json({ message: medicamentos.message });
       }
-  
-      // Retornamos los medicamentos encontrados
       res.json(medicamentos);
-  
     } catch (error) {
-      // Manejo de errores en la ruta
       console.error(error);
       res.status(500).json({ error: 'Hubo un error al obtener los medicamentos de tipo VACUNA.' });
     }
   });
 
 
-  // Ruta para obtener los medicamentos de tipo SUPRESOR de una mascota por id_mascota
+// Ruta para obtener los medicamentos de tipo SUPRESOR de una mascota por id_mascota
 router.get('/supresor/:id_mascota', async (req, res) => {
-  const { id_mascota } = req.params;  // Obtenemos el id_mascota desde los parámetros de la URL
-  
+  const { id_mascota } = req.params;  
   try {
-    // Llamamos a la función para obtener los medicamentos de tipo VACUNA para la mascota
     const medicamentos = await MascotasMedicamentosQueries.obtenerMedicamentosPorMascotaSupresor(id_mascota);
-
     if (medicamentos.message) {
-      // Si no se encontraron resultados
       return res.status(404).json({ message: medicamentos.message });
     }
-
-    // Retornamos los medicamentos encontrados
     res.json(medicamentos);
-
   } catch (error) {
-    // Manejo de errores en la ruta
     console.error(error);
     res.status(500).json({ error: 'Hubo un error al obtener los medicamentos de tipo VACUNA.' });
   }
 });
-
 
 // Ruta para obtener los medicamentos de tipo DESPARASITANTE de una mascota por id_mascota
 router.get('/desparasitante/:id_mascota', async (req, res) => {
-  const { id_mascota } = req.params;  // Obtenemos el id_mascota desde los parámetros de la URL
+  const { id_mascota } = req.params; 
   
   try {
-    // Llamamos a la función para obtener los medicamentos de tipo VACUNA para la mascota
     const medicamentos = await MascotasMedicamentosQueries.obtenerMedicamentosPorMascotaDesparasitante(id_mascota);
-
     if (medicamentos.message) {
-      // Si no se encontraron resultados
       return res.status(404).json({ message: medicamentos.message });
     }
-
-    // Retornamos los medicamentos encontrados
     res.json(medicamentos);
 
   } catch (error) {
-    // Manejo de errores en la ruta
     console.error(error);
     res.status(500).json({ error: 'Hubo un error al obtener los medicamentos de tipo VACUNA.' });
   }
 });
+
+// Ruta para crear una nueva  mascota_medicamento
+router.post('/', async (req, res) => {
+  try {
+    const { id_mascota, id_medicamento, tipo, cantidad, fecha_dosis, fecha_refuerzo } = req.body;
+    const nuevoMascotaMedicamento = await MascotasMedicamentosQueries.createMascotaMedicamento(
+      id_mascota, id_medicamento, tipo, cantidad, fecha_dosis, fecha_refuerzo
+    );
+    res.status(201).json(nuevoMascotaMedicamento);  
+  } catch (error) {
+    console.error('Error al crear la relación entre mascota y medicamento:', error);
+    res.status(500).json({ error: 'Error al crear la relación entre mascota y medicamento' });
+  }
+});
+
+// Ruta para actualizar una  mascota_medicamento
+router.put('/:id_mascotaMedicamento', async (req, res) => {
+  try {
+    const { id_mascotaMedicamento } = req.params;
+    const { id_mascota, id_medicamento, tipo, cantidad, fecha_dosis, fecha_refuerzo } = req.body;
+    const resultado = await MascotasMedicamentosQueries.updateMascotaMedicamento(
+      id_mascotaMedicamento, id_mascota, id_medicamento, tipo, cantidad, fecha_dosis, fecha_refuerzo
+    );
+    res.json(resultado);  
+  } catch (error) {
+    console.error('Error al actualizar la relación entre mascota y medicamento:', error);
+    res.status(500).json({ error: 'Error al actualizar la relación entre mascota y medicamento' });
+  }
+});
+
+// Ruta para eliminar una mascota_medicamento
+router.delete('/:id_mascotaMedicamento', async (req, res) => {
+  try {
+    const { id_mascotaMedicamento } = req.params;      
+    const resultado = await MascotasMedicamentosQueries.deleteMascotaMedicamento(id_mascotaMedicamento);
+    if (resultado.message === 'Relación entre mascota y medicamento eliminada con éxito') {
+      res.status(200).json(resultado);  
+    } else {
+      res.status(404).json(resultado);  
+    }
+  } catch (error) {
+    console.error('Error al eliminar la relación entre mascota y medicamento:', error);
+    res.status(500).json({ message: 'Error al eliminar la relación entre mascota y medicamento', error: error.message });
+  }
+});
+
 
 module.exports = router;
